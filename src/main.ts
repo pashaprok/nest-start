8a0b4891f133b90c9da773b294fe169025a31b5d
Prokopuk Pavel <pashaprok23@gmail.com>
2022-05-24 16:36:39 +0300

    add swagger, logger and config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // app init
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('NestNotes', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
+  // define server port
+  const PORT = +process.env.PORT || 5000;
+
+  // OpenApi init
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestStart')
+    .setDescription('REST API Documentation')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+  );
+  SwaggerModule.setup('/api/docs', app, document);
+
+  // listen application at defined port
+  await app.listen(PORT);
+  Logger.log(`API listening at http://localhost:${PORT}/`);
+  Logger.log(`API docs at http://localhost:${PORT}/api/docs`);
 }
+
+// nest start
 bootstrap();
